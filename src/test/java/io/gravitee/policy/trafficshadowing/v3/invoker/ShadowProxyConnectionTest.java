b539f7ba293d75bd7d587f7bead08633e1ed66fe
Julien Giovaresco <dev@giovaresco.fr>
2025-01-09 15:37:18 +0100

    refactor: extract v3 implementation

--- src/test/java/io/gravitee/policy/trafficshadowing/v3/invoker/ShadowProxyConnectionTest.java
+++ src/test/java/io/gravitee/policy/trafficshadowing/v3/invoker/ShadowProxyConnectionTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.policy.trafficshadowing.invoker;
+package io.gravitee.policy.trafficshadowing.v3.invoker;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
